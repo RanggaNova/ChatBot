012d767a8542b4bddc8cf7b0f3f4717b877f2053
rangganova <ranggaamazon2@gmail.com>
2025-07-17 12:53:39 +0700

    judul

--- APP.py
+++ APP.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from Inference import chat_bot
 
-st.title("Chat with Gemini 2.0 Flash")
+st.title("Chat bot dengan api gemini")
 
 if"message" not in st.session_state:
     st.session_state.message = []
